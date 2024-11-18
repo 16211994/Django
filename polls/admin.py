c6e42fd1868508e5c6c78065064e6dbe1ed0c64a
16211994 <cakek433@gmail.com>
2024-11-19 01:24:43 +0300

    Adding two class one for Choice and one for Question

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from .models import Choice, Question
 
-from .models import Question
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
