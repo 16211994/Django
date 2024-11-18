f2cf4a76309797266796e0fcf0539ac1041caaa5
16211994 <cakek433@gmail.com>
2024-11-19 02:02:21 +0300

    improve string represenation the output using diplay() decorator

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200, default="Default Question Text")
@@ -9,9 +10,15 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
-        return self.pub_date <= now and now - datetime.timedelta(days=1) <= self.pub_date
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     # def was_published_recently(self):
     #    now = timezone.now()
     # # Check if the publication date is in the future
