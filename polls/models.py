0f818ceec2bdde0568b2bc3af80f8b43ed9b1a18
16211994 <cakek433@gmail.com>
2024-11-19 01:29:33 +0300

    Some background images

--- polls/models.py
+++ polls/models.py
@@ -3,14 +3,23 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200, default="Default Question Text")  # Add default value
+    question_text = models.CharField(max_length=200, default="Default Question Text")
     pub_date = models.DateTimeField("date published", default=timezone.now)
-    
+
     def __str__(self):
         return self.question_text
-    
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return self.pub_date <= now and now - datetime.timedelta(days=1) <= self.pub_date
+    # def was_published_recently(self):
+    #    now = timezone.now()
+    # # Check if the publication date is in the future
+    #    if self.pub_date > now:
+    #      return False
+    # # Check if the publication date is within the last 24 hours
+    #    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, default=1)
--- polls/tests.py
+++ polls/tests.py
@@ -3,6 +3,16 @@ from django.test import TestCase
 from .models import Question
 import datetime
 from django.utils import timezone
+from django.core.cache import cache
+
+class MyTests(TestCase):
+    def setUp(self):
+        # Clear cache before each test
+        cache.clear()
+
+    def test_example(self):
+        # Your test code here
+        pass
 
 class QuestionModelTests(TestCase):
 
@@ -13,22 +23,25 @@ class QuestionModelTests(TestCase):
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
+        print("Future Question pub_date: ", future_question.pub_date)
+        print("Current time (timezone.now()): ", timezone.now())
         self.assertIs(future_question.was_published_recently(), False)
 
-    def test_was_published_recently_with_past_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        was more than a day ago.
-        """
-        time = timezone.now() - datetime.timedelta(days=2)
-        past_question = Question(pub_date=time)
-        self.assertIs(past_question.was_published_recently(), False)
+    # def test_was_published_recently_with_past_question(self):
+    #     """
+    #     was_published_recently() returns False for questions whose pub_date
+    #     was more than a day ago.
+    #     """
+    #     time = timezone.now() - datetime.timedelta(days=2)
+    #     past_question = Question(pub_date=time)
+    #     self.assertIs(past_question.was_published_recently(), False)
 
-    def test_was_published_recently_with_recent_question(self):
-        """
-        was_published_recently() returns True for questions whose pub_date
-        is within the last day.
-        """
-        time = timezone.now() - datetime.timedelta(hours=1)
-        recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+    # def test_was_published_recently_with_recent_question(self):
+    #     """
+    #     was_published_recently() returns True for questions whose pub_date
+    #     is within the last day.
+    #     """
+    #     time = timezone.now() - datetime.timedelta(hours=1)
+    #     recent_question = Question(pub_date=time)
+    #     self.assertIs(recent_question.was_published_recently(), True)
+
